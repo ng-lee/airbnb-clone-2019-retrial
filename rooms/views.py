14fd2bb7ffa4c49b2060914b2339f69d94a3890b
codeer <loginonlyyy@gmail.com>
2022-02-04 18:09:07 +0900

    showed limited rooms

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,9 @@ from . import models as room_models
 
 
 def all_rooms(request):
-    all_rooms = room_models.Room.objects.all()
+    page = int(request.GET.get("p", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = room_models.Room.objects.all()[offset:limit]
     return render(request, "home.html", context={"rooms": all_rooms})
