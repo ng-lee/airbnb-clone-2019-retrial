8b27d5a1c6a119a6c037067504dfdc8283264cbb
codeer <loginonlyyy@gmail.com>
2022-02-06 21:20:51 +0900

    created search form part Two

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,9 +1,29 @@
 from django import forms
+from django_countries.fields import CountryField
 from rooms import models as room_models
 
 
 class SearchForm(forms.Form):
 
-    city = forms.CharField()
-    price = forms.IntegerField()
-    room_type = forms.ModelChoiceField(queryset=room_models.RoomType.objects.all())
+    city = forms.CharField(initial="Anywhere")
+    country = CountryField(default="KR").formfield()
+    room_type = forms.ModelChoiceField(
+        required=False, queryset=room_models.RoomType.objects.all()
+    )
+    price = forms.IntegerField(required=False)
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+    amenities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=room_models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    facilities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=room_models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
--- rooms/views.py
+++ rooms/views.py
@@ -21,6 +21,67 @@ class RoomDetailView(DetailView):
 
 def search(request):
 
-    form = forms.SearchForm()
+    city = request.GET.get("city")
+    rooms = None
 
-    return render(request, "rooms/search.html", context={"form": form})
+    if city:
+        form = forms.SearchForm(request.GET)
+
+        if form.is_valid():
+            city = form.cleaned_data.get("city")
+            country = form.cleaned_data.get("country")
+            room_type = form.cleaned_data.get("room_type")
+            price = form.cleaned_data.get("price")
+            guests = form.cleaned_data.get("guests")
+            bedrooms = form.cleaned_data.get("bedrooms")
+            beds = form.cleaned_data.get("beds")
+            baths = form.cleaned_data.get("baths")
+            instant_book = form.cleaned_data.get("instant_book")
+            superhost = form.cleaned_data.get("superhost")
+            amenities = form.cleaned_data.get("amenities")
+            facilities = form.cleaned_data.get("facilities")
+
+            filter_args = {}
+
+            if city != "Anywhere":
+                filter_args["city__startswith"] = city
+
+            filter_args["country"] = country
+
+            if room_type is not None:
+                filter_args["room_type"] = room_type
+
+            if price is not None:
+                filter_args["price__lte"] = price
+
+            if guests is not None:
+                filter_args["guests__gte"] = guests
+
+            if bedrooms is not None:
+                filter_args["bedrooms__gte"] = bedrooms
+
+            if beds is not None:
+                filter_args["beds__gte"] = beds
+
+            if baths is not None:
+                filter_args["baths__gte"] = baths
+
+            if instant_book is True:
+                filter_args["instant_book"] = True
+
+            if superhost is True:
+                filter_args["host__superhost"] = True
+
+            rooms = room_models.Room.objects.filter(**filter_args)
+
+            for amenity in amenities:
+                rooms = rooms.filter(amenities=amenity)
+
+            for facility in facilities:
+                rooms = rooms.filter(facilities=facility)
+
+    else:
+
+        form = forms.SearchForm()
+
+    return render(request, "rooms/search.html", context={"form": form, "rooms": rooms})
