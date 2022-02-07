b565a77beefa728f8821526ab007251067e2292a
codeer <loginonlyyy@gmail.com>
2022-02-07 20:52:28 +0900

    email and pw validation

--- users/forms.py
+++ users/forms.py
@@ -7,12 +7,14 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
         try:
-            models.User.objects.get(username=email)
-            return email
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", "Wrong Password!")
         except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
-
-    
+            self.add_error("email", "User does not exist!")
