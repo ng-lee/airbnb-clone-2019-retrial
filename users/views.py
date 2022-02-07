c3fa86650cdc263f23e6e5c5a082f7218fd7842d
codeer <loginonlyyy@gmail.com>
2022-02-07 18:14:24 +0900

    login form and csrf

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from users.forms import LoginForm
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, "users/login.html", context={"form": form})
+
+    def post(self, request):
+        pass
