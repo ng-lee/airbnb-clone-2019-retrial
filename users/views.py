6d617f304ac13402a32039ffb98917b87a68856f
codeer <loginonlyyy@gmail.com>
2022-02-14 21:28:38 +0900

    github login part One

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth import authenticate, login, logout
@@ -42,3 +43,15 @@ class SignupView(FormView):
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
+
+
+def github_login(request):
+    client_id = os.environ.get("GH_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}"
+    )
+
+
+def github_callback(request):
+    pass
