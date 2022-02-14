f9d30247422e623248bd109b1c4d31dacd2b0246
codeer <loginonlyyy@gmail.com>
2022-02-14 22:25:17 +0900

    getting github profile

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from django.views.generic import FormView
-from . import forms
+from . import forms, models
 
 
 class LoginView(View):
@@ -59,10 +59,42 @@ def github_callback(request):
     client_secret = os.environ.get("GH_SECRET")
     code = request.GET.get("code", None)
     if code is not None:
-        re = requests.post(
+        result_request = requests.post(
             f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
             headers={"Accept": "application/json"},
         )
-        print(re.json())
+        result_json = result_request.json()
+        error = result_json.get("error", None)
+        if error is not None:
+            return redirect(reverse("core:home"))
+        else:
+            token = result_json.get("access_token")
+            profile_request = requests.get(
+                "https://api.github.com/user",
+                headers={
+                    "Authorization": f"token {token}",
+                    "Accept": "application/json",
+                },
+            )
+            profile_json = profile_request.json()
+            username = profile_json.get("login", None)
+            if username is not None:
+                name = profile_json.get("name")
+                email = profile_json.get("email")
+                bio = profile_json.get("bio")
+                user = models.User.objects.get(username=username)
+                if user is not None:
+                    login(request, user)
+                else:
+                    user = models.User.objects.create(
+                        username=username,
+                        first_name=name,
+                        email=email,
+                        bio=bio,
+                        login_method=models.User.LOGIN_GITHUB,
+                    )
+            else:
+                return redirect(reverse("core:home"))
+
     else:
         return redirect(reverse("core:home"))
