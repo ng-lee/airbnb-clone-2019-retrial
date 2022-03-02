41121c3e78936caf559e2f39677ab8a18b63927d
codeer <loginonlyyy@gmail.com>
2022-03-02 22:12:43 +0900

    getting kakao profile image

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ import requests
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth import authenticate, login, logout
+from django.core.files.base import ContentFile
 from django.views import View
 from django.views.generic import FormView
 from . import forms, models
@@ -161,6 +162,11 @@ def kakao_callback(request):
                 )
                 user.set_unusable_password()
                 user.save()
+                if profile_image is not None:
+                    photo_request = requests.get(profile_image)
+                    user.avatar.save(
+                        f"{nickname}-avatar", ContentFile(photo_request.content)
+                    )
             login(request, user)
             return redirect(reverse("core:home"))
     except KakaoException:
