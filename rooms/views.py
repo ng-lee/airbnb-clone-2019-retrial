33762bf73456aa8aad9a0bf1962f831ddbee8d55
codeer <loginonlyyy@gmail.com>
2022-03-06 15:13:10 +0900

    room cards part Two

--- rooms/views.py
+++ rooms/views.py
@@ -7,7 +7,7 @@ class HomeView(ListView):
 
     model = room_models.Room
     page_kwarg = "p"
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 5
     context_object_name = "rooms"
     template_name = "home.html"
