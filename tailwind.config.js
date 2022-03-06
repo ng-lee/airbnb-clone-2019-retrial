48fede856b5dfdfcdb60104156730889f9f5ec73
codeer <loginonlyyy@gmail.com>
2022-03-06 17:26:06 +0900

    footer social login

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         "50vh": "50vh",
         "75vh": "75vh",
       },
+      colors: {
+        "airbnb-pink": "#FF5A5F",
+        "airbnb-green": "rgb(45, 212, 191)",
+        "github-gray": "#181717",
+        "kakao-yellow": "#FFCD00",
+      },
     },
   },
   plugins: [],
